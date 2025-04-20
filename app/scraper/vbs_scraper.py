1d48c113640fc5c392656bd8e0f548bf8993e316
iantato <83338977+iantato@users.noreply.github.com>
2025-04-20 19:14:39 +0800

    Updated the VBS Scraper.

--- app/scraper/vbs_scraper.py
+++ app/scraper/vbs_scraper.py
@@ -1,3 +1,6 @@
+import shutil
+from os import path
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
@@ -6,8 +9,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from app.scraper.driver import Driver
 from app.config.logger import setup_logger
+from app.config.constants import DATA_DIR
 from app.models.scraper import (
-    Account
+    Account,
+    Dates
+)
+from app.utils.directory import (
+    wait_for_download,
+    create_save_directory
 )
 from app.utils.exceptions import (
     LoginFailedException,
@@ -18,30 +27,29 @@ logger = setup_logger(__name__)
 
 class VBSScraper:
 
-    @staticmethod
+    def __init__(self):
+        self.url = 'https://vbs.1-stop.biz'
+
     def authenticate(self, account: Account) -> bool:
         """
-            Authenticate the user with the VBS system using the provided credentials.
-            This method uses the Selenium WebDriver to interact with the VBS login page.
-            It waits for the page to load and checks for the presence of specific elements
-            to determine if the login was successful.
+        Authenticate the user with the VBS system using the provided credentials.
+        This method uses the Selenium WebDriver to interact with the VBS login page.
+        It waits for the page to load and checks for the presence of specific elements
+        to determine if the login was successful.
 
-            Parameters:
-                account (Account): The account object containing the username and password.
+        Parameters:
+            account (Account): The account object containing the username and password.
 
-            Returns:
-                bool: True if login was successful, False otherwise.
+        Returns:
+            bool: True if login was successful, False otherwise.
 
-            Raises:
-                LoginFailedException: If the login fails due to incorrect credentials.
-                LoadingFailedException: If the page takes too long to load or elements are not found.
+        Raises:
+            LoginFailedException: If the login fails due to incorrect credentials.
+            LoadingFailedException: If the page takes too long to load or elements are not found.
         """
-
-        url = 'https://ictsi.vbs.1-stop.biz'
-
         with Driver() as (driver, wait):
             try:
-                driver.get(url)
+                driver.get(self.url)
 
                 wait.until(EC.all_of(
                     EC.visibility_of_element_located((By.ID, 'username')),
@@ -63,20 +71,19 @@ class VBSScraper:
                 logger.error('Timed out. The VBS page took too long to load.')
                 raise LoadingFailedException('Timed out. The VBS page took too long to load.')
 
-    @staticmethod
-    def _verify_login(driver: Chrome, wait: WebDriverWait) -> bool:
+    def _verify_login(self, driver: Chrome, wait: WebDriverWait) -> bool:
         """
-            Verify if the login was successful by checking for the presence of specific elements.
-            This method checks for the presence of an error message element that indicates a failed login.
+        Verify if the login was successful by checking for the presence of specific elements.
+        This method checks for the presence of an error message element that indicates a failed login.
 
-            If the error message is not found, it assumes the login was successful.
+        If the error message is not found, it assumes the login was successful.
 
-            Parameters:
-                driver (Chrome): The Selenium WebDriver instance.
-                wait (WebDriverWait): The WebDriverWait instance for waiting for elements.
+        Parameters:
+            driver (Chrome): The Selenium WebDriver instance.
+            wait (WebDriverWait): The WebDriverWait instance for waiting for elements.
 
-            Returns:
-                bool: True if login was successful, False otherwise.
+        Returns:
+            bool: True if login was successful, False otherwise.
         """
         try:
             wait.until(EC.presence_of_element_located((By.ID, 'error-element-password')))
@@ -104,6 +111,15 @@ class VBSScraper:
         return save_dir
 
     def _accept_terms_and_conditions(self, driver: Chrome, wait: WebDriverWait, company: str) -> None:
+        """
+        Accept the terms and conditions for the specified company.
+        This method navigates to the terms and conditions page and clicks the accept button.
+
+        Parameters:
+            driver (Chrome): The Selenium WebDriver instance.
+            wait (WebDriverWait): The WebDriverWait instance for waiting for elements.
+            company (str): The company name to be used in the URL.
+        """
 
         # Go to the terms and conditions page.
         # We need to get rid of the 'https://' part of the URL to get the company
@@ -115,10 +131,36 @@ class VBSScraper:
         driver.get(f'https://{company.lower()}{self.url[8:]}/PointsTransactions.aspx')
 
     def _move_download_file(self, src_filename: str, dest_dir: str, dest_filename: str) -> None:
-        pass
+        """
+        Move the downloaded file from the source directory to the save directory.
+
+        Parameters:
+            src_filename (str): The name of the source file to be moved.
+            dest_dir (str): The destination directory where the file will be moved.
+            dest_filename (str): The new name for the file in the destination directory.
+        """
+        src_path = path.join(DATA_DIR, src_filename)
+        dest_path = path.join(DATA_DIR, dest_dir, dest_filename)
+
+        shutil.move(src_path, dest_path)
+        logger.info(f'File moved to {dest_path}')
 
     def download_data(self, driver: Chrome, wait: WebDriverWait, account: Account, dates: Dates,
-                      company: str, csv_filename: str, save_dir: str) -> None:
+                      company: str, csv_filename: str) -> None:
+        """
+        Download data from the VBS system using the provided account credentials and date range.
+        This method uses the Selenium WebDriver to interact with the VBS system and download the data.
+
+        Parameters:
+            driver (Chrome): The Selenium WebDriver instance.
+            wait (WebDriverWait): The WebDriverWait instance for waiting for elements.
+            account (Account): The account object containing the username and password.
+            dates (Dates): The Dates object containing the start and end dates.
+            company (str): The company name to be used in the URL.
+            csv_filename (str): The name of the CSV file to be downloaded.
+        """
+
+        save_dir = self._generate_save_directory(dates)
 
         with Driver() as (driver, wait):
             try:
@@ -160,7 +202,7 @@ class VBSScraper:
                 driver.find_element(By.ID, 'Search').click()
 
                 if wait_for_download('PointsTransactions.csv'):
-                    pass
+                    self._move_download_file('PointsTransactions.csv', save_dir, csv_filename)
 
             except TimeoutException or NoSuchElementException:
                 logger.error('Timed out. The page took too long to load.')
